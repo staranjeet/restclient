bcbfad9a594c2a3d943f23a8ad1cba4781ca818f
Taranjeet <reachtotj@gmail.com>
2015-09-10 08:51:28 +0530

    urls changed

--- restclient/djangorestclient/urls.py
+++ restclient/djangorestclient/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls.static        import static
 from django.conf.urls 	import url
 
 from .views 			import DjangoRestClient
--- restclient/urls.py
+++ restclient/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static        import static
+from django.conf	import settings
+
+from djangorestclient.views import DjangoRestClient
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'restclient.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', include('djangorestclient.urls')),
+    url(r'^d/',DjangoRestClient.as_view(),name="django-rest-client-view"),
 
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
