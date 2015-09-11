5311e53d07f9689e9c873d617c6b0e5c30dbc1bb
Taranjeet <reachtotj@gmail.com>
2015-09-11 08:53:31 +0530

    basic template added

--- restclient/settings.py
+++ restclient/settings.py
@@ -112,3 +112,22 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     "django.contrib.staticfiles.finders.FileSystemFinder",
     )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ os.path.join(PROJECT_PATH, "templates"), ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                "django.core.context_processors.request",
+                'django.core.context_processors.media',
+                'django.core.context_processors.static',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
